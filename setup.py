87e2dbd28d5baf6afcf758930174459b336be78e
Edward <emorg31@lsu.edu>
2024-05-15 12:43:19 -0500

    build config fixes

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/Eddy-Morgan/Diff_UV.git",  # Optional
+    url="https://github.com/Eddy-Morgan/Diff_UV",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
     author="Edward Morgan",  # Optional
